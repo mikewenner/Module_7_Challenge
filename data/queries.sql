85d8f04b883fb693f5093d9e7ca69e34702b879a
mikewenner <mikewenner121@gmail.com>
2023-02-13 12:03:15 -0500

    2/13 finished queries

--- data/queries.sql
+++ data/queries.sql
@@ -26,8 +26,13 @@ INNER JOIN credit_card as b ON a.id = b.cardholder_id
 INNER JOIN transaction as c ON b.card = c.card
 ORDER BY a.name
 
-
 -- 2.  Count the transactions that are less than $2.00 per cardholder.
+SELECT
+count(c.amount),
+count(c.id)
+FROM transaction AS c
+WHERE c.amount < 2.00
+-- with names
 SELECT 
 a.name,
 a.id,
@@ -38,13 +43,11 @@ INNER JOIN credit_card as b ON a.id = b.cardholder_id
 INNER JOIN transaction as c ON b.card = c.card
 WHERE c.amount < 2.00
 ORDER BY a.name
--- GROUP BY a.name
--- ORDER BY count(*)
 
 -- 3.  Is there any evidence to suggest that a credit card has been hacked? Explain your rationale.
 --		If we define several as anything greater than 3, then yes based on just having several transactions less than 2.00 
--- 		then yes there is evidence of credit cards being hacked for all customerrs except Elizabeth Sawyer and Laurie Gibbs
--- 		who each had only 3 transactions less than 2.00
+-- 		then yes there is evidence of credit cards being hacked for all customers except Elizabeth Sawyer and Laurie Gibbs
+-- 		who each had only 3 transactions less than 2.00.
 
 -- 4.  What are the top 100 highest transactions made between 7:00 am and 9:00 am?
 select * from transaction
@@ -65,24 +68,22 @@ and amount < 2.00
 select count(amount) from transaction
 WHERE amount < 2.00
 
--- no, there is 15 transactions between 7 and 9 am and 350 total trtansactions less than 2.00.  So that means there is 
--- 335 transactions during the rest of the day that are less than 2.00 and could be considered fraud
-
---select count(amount) from transaction
---WHERE date_part('hour', date) > 9 
---and date_part('hour', date) < 7
---and amount < 2.00
+-- no, there is 15 transactions between 7 and 9 am and 350 total transactions less than 2.00.  So that means there is 
+-- 335 transactions during the rest of the day that are less than 2.00 and could be considered fraud.
 
 -- What are the top 5 merchants prone to being hacked using small transactions?
 SELECT 
 a.id,
 a.name,
-b.amount
+count(b.amount) as amount_count
 FROM merchant as a
 INNER JOIN transaction as b ON a.id = b.id_merchant
 WHERE b.amount < 2.00
-GROUP BY a.name
-ORDER BY count(*) DESC;
+GROUP BY a.id, a.name
+ORDER BY amount_count DESC
+LIMIT 5
+-- Based on the above query, the top 5 merchants prone to being hacked based on transactions being less thn 2.00 are:
+-- Wood-Ramirez (7), Hood-Phillips (6), Baker Inc (6), Greene-Wood (5) & Jarvis-Turner (5).
 
 -- PART 2
 --cardholder IDs are 2 and 18.
